5f17c23b317e7031e3d66725ac94460319d91a8f
Blake McDonald <56270953+brm7231@users.noreply.github.com>
2022-06-01 14:07:27 -0500

    Speed fix and Projects

--- nav.js
+++ nav.js
@@ -3,7 +3,6 @@ fetch('../nav.html')
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
-    newelem.className = "container";
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
