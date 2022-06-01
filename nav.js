e91c976e6d9cc1305cd4012634a833929cba668a
Blake McDonald <56270953+brm7231@users.noreply.github.com>
2022-06-01 13:12:54 -0500

    tes

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ fetch('../nav.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div.container");
+    let newelem = document.createElement("div class='container'");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
