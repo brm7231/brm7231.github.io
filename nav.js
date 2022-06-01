fe2d2020d9b1ec1ec5d63d2f5b3a6ee19cf64832
Blake McDonald <56270953+brm7231@users.noreply.github.com>
2022-06-01 13:22:30 -0500

    newest test

--- nav.js
+++ nav.js
@@ -2,7 +2,8 @@ fetch('../nav.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div").classList.add("container");
+    let newelem = document.createElement("div");
+    newelem.className = "container";
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
