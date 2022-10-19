4b14011628fd2f53982459a857a8d74eb95f0da9
Blake McDonald <56270953+brm7231@users.noreply.github.com>
2022-10-19 02:37:35 -0500

    edit again

--- preloader.js
+++ preloader.js
@@ -5,6 +5,4 @@ fetch('../preloader')
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
-    document.querySelector("head").appendChild("<script src='/preloader/script.js'></script>")
-    document.querySelector("head").appendChild("<style src='/preloader/style.css'></style>")
 })
